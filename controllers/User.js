7f44e9d9ce4b335ffb821db629221e8d24db0914
Jercode <maguyonj1@gmail.com>
2024-09-18 15:49:14 +0800

    Switch to bcryptjs for compatibility

--- controllers/User.js
+++ controllers/User.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const User = require("../models/User");
 const auth = require("../auth");
 const { errorHandler } = require("../auth");
